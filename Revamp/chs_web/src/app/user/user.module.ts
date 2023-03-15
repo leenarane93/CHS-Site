a849a16efdd414fdb7b500723cba82e7cd58919f
leenarane93 <leenarane93@outlook.com>
2023-03-15 21:38:50 +0530

    First Commit

--- Revamp/chs_web/src/app/user/user.module.ts
+++ Revamp/chs_web/src/app/user/user.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserLayoutComponent } from './user-layout/user-layout.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserLayoutComponent
+  ],
   imports: [
     CommonModule
   ]
